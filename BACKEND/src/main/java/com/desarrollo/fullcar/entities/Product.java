99067d8a1251a8f6bb132fe64a796b6c82c484e0
javier <javiermanesero@gmail.com>
2022-09-10 19:37:47 -0300

    Clase Producto subida,y archivo base de datos

--- BACKEND/src/main/java/com/desarrollo/fullcar/entities/Product.java
+++ BACKEND/src/main/java/com/desarrollo/fullcar/entities/Product.java
@@ -1,4 +1,59 @@
 package com.desarrollo.fullcar.entities;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+import javax.persistence.Table;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name= "product")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 
 public class Product {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(name="nombre")
+        private String nombre;
+        @Column(name="descripcion")
+        private String descripcion;
+        @Column(name="importado")
+        private boolean importado;
+        @Column(name="marca")
+        private String marca;
+        @Column(name="modelo")
+        private int modelo;
+        @Column(name="precio")
+        private float precio;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "fk_categoria", nullable = false)
+    private Categoria categoria;
+
+
+
+
 }
